40f58b4cfffce60da232b6f9fde950cbe5e4a0e9
chithian <chithiansamony@gmail.com>
2022-10-08 11:52:20 +0700

    Change Name Folder & fix controller

--- ___FILEBASENAME___Controller/Controller/___FILEBASENAME___ViewController.swift
+++ ___FILEBASENAME___Controller/Controller/___FILEBASENAME___ViewController.swift
@@ -3,28 +3,10 @@
 import UIKit
 
 class ___FILEBASENAME___: BaseViewController<___FILEBASENAME___View, ___FILEBASENAME___ViewModel> {
-
-//    let loginView = LoginView()
-//    let viewModel = LoginViewModel()
-//
-//    override func loadView() {
-//        super.loadView()
-//
-//        view = loginView
-//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
         
-        listener()
-        viewModel.requestApi(id: 2)
     }
-    
-    private func listener()
-    {
-        viewModel.result = { result in
-            self.log(tag: "LoginViewController", msg: " \(result)")
-        }
-    }
+
 }
